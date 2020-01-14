d31e293d74bf08dc04dd78d17a99b3520c035f71
Melissa Tranfield <mrotranfield@gmail.com>
2020-01-14 12:53:50 +0000

    add highest, lowest and divide functions

--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -20,4 +20,24 @@ describe("Coolculator", function() {
     result = mm.subtract(3, 2);
     expect(result).to.equal(1);
   });
+
+  it("should divide", () => {
+    result = mm.divide(20, 5);
+    expect(result).to.equal(4);
+  })
+
+  it("should return 'operation not possible' when trying to divide by 0", () => {
+    result = mm.divide(20, 0);
+    expect(result).to.equal('operation not possible');
+  })
+  
+  it("should return highest of 2 integers", () => {
+    result = mm.highest(20, 30);
+    expect(result).to.equal(30);
+  })
+
+  it("should return lowest of 2 integers", () => {
+    result = mm.lowest(20, 30);
+    expect(result).to.equal(20);
+  })
 });
